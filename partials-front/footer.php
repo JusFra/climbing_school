24b29aadfcb4ec58f9685f766419005f4d4e7488
Justyna <justa.frankiewicz@gmail.com>
2022-05-20 15:18:27 +0200

    Menu and footer - frontend

---
+++ partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- Social Section Starts -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluency/48/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/instagram-new--v1.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/color/48/000000/tiktok--v1.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- Social Section Ends --> 
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Justyna Frankiewicz</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,45 @@
+<?php include('config/constants.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Climbing School Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="<?php echo SITEURL; ?>" title="Logo">
+                    <img src="images/logo3.png" alt="Climbing school logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>categories.php">Courses & Camps</a>
+                    </li>
+                    <li>
+                        <a href="#">Our Staff</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
